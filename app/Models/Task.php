428ab74828891af11bb2f486c5d998e14cdf940e
harshil <hrachchh7@gmail.com>
2025-09-30 00:35:50 +0530

    setup relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Task extends Model
 {
     use SoftDeletes;
+    protected $fillable = ['project_id', 'name', 'description', 'status', 'due_date', 'priority'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
